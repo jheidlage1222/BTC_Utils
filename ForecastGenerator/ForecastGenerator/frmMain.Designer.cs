347ac53c843d9505f1c2effa805fbc172c791826
John Heidlage <john.heidlage@gmail.com>
2017-12-13 19:51:11 -0500

    Initial load of forecast program and history data file in a directory that will eventually host my control app

---
+++ ForecastGenerator/ForecastGenerator/frmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ForecastGenerator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHistoryFilePath = new System.Windows.Forms.TextBox();
+            this.lblHistDataPath = new System.Windows.Forms.Label();
+            this.btnDoCalcs = new System.Windows.Forms.Button();
+            this.gbOutput = new System.Windows.Forms.GroupBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.gbParamCtrls = new System.Windows.Forms.GroupBox();
+            this.tBoxPrincipalAmt = new System.Windows.Forms.TextBox();
+            this.tBoxPurchaseFee = new System.Windows.Forms.TextBox();
+            this.tBoxSellFee = new System.Windows.Forms.TextBox();
+            this.gbOutput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.gbParamCtrls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtHistoryFilePath
+            // 
+            this.txtHistoryFilePath.Location = new System.Drawing.Point(39, 44);
+            this.txtHistoryFilePath.Name = "txtHistoryFilePath";
+            this.txtHistoryFilePath.Size = new System.Drawing.Size(421, 20);
+            this.txtHistoryFilePath.TabIndex = 0;
+            // 
+            // lblHistDataPath
+            // 
+            this.lblHistDataPath.AutoSize = true;
+            this.lblHistDataPath.Location = new System.Drawing.Point(39, 25);
+            this.lblHistDataPath.Name = "lblHistDataPath";
+            this.lblHistDataPath.Size = new System.Drawing.Size(100, 13);
+            this.lblHistDataPath.TabIndex = 1;
+            this.lblHistDataPath.Text = "Path to History CSV";
+            // 
+            // btnDoCalcs
+            // 
+            this.btnDoCalcs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoCalcs.Location = new System.Drawing.Point(495, 44);
+            this.btnDoCalcs.Name = "btnDoCalcs";
+            this.btnDoCalcs.Size = new System.Drawing.Size(167, 60);
+            this.btnDoCalcs.TabIndex = 2;
+            this.btnDoCalcs.Text = "Generate Forecasts";
+            this.btnDoCalcs.UseVisualStyleBackColor = true;
+            // 
+            // gbOutput
+            // 
+            this.gbOutput.Controls.Add(this.dgvData);
+            this.gbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbOutput.Location = new System.Drawing.Point(57, 379);
+            this.gbOutput.Name = "gbOutput";
+            this.gbOutput.Size = new System.Drawing.Size(824, 176);
+            this.gbOutput.TabIndex = 3;
+            this.gbOutput.TabStop = false;
+            this.gbOutput.Text = "Output Data";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvData.Location = new System.Drawing.Point(3, 19);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(818, 154);
+            this.dgvData.TabIndex = 0;
+            // 
+            // gbParamCtrls
+            // 
+            this.gbParamCtrls.Controls.Add(this.tBoxSellFee);
+            this.gbParamCtrls.Controls.Add(this.tBoxPurchaseFee);
+            this.gbParamCtrls.Controls.Add(this.tBoxPrincipalAmt);
+            this.gbParamCtrls.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbParamCtrls.Location = new System.Drawing.Point(57, 179);
+            this.gbParamCtrls.Name = "gbParamCtrls";
+            this.gbParamCtrls.Size = new System.Drawing.Size(824, 176);
+            this.gbParamCtrls.TabIndex = 4;
+            this.gbParamCtrls.TabStop = false;
+            this.gbParamCtrls.Text = "Input Params";
+            // 
+            // tBoxPrincipalAmt
+            // 
+            this.tBoxPrincipalAmt.Location = new System.Drawing.Point(7, 118);
+            this.tBoxPrincipalAmt.Name = "tBoxPrincipalAmt";
+            this.tBoxPrincipalAmt.Size = new System.Drawing.Size(100, 23);
+            this.tBoxPrincipalAmt.TabIndex = 0;
+            // 
+            // tBoxPurchaseFee
+            // 
+            this.tBoxPurchaseFee.Location = new System.Drawing.Point(137, 118);
+            this.tBoxPurchaseFee.Name = "tBoxPurchaseFee";
+            this.tBoxPurchaseFee.Size = new System.Drawing.Size(100, 23);
+            this.tBoxPurchaseFee.TabIndex = 1;
+            // 
+            // tBoxSellFee
+            // 
+            this.tBoxSellFee.Location = new System.Drawing.Point(276, 118);
+            this.tBoxSellFee.Name = "tBoxSellFee";
+            this.tBoxSellFee.Size = new System.Drawing.Size(100, 23);
+            this.tBoxSellFee.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 588);
+            this.Controls.Add(this.gbParamCtrls);
+            this.Controls.Add(this.gbOutput);
+            this.Controls.Add(this.btnDoCalcs);
+            this.Controls.Add(this.lblHistDataPath);
+            this.Controls.Add(this.txtHistoryFilePath);
+            this.Name = "frmMain";
+            this.Text = "Forecast Generator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.gbOutput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.gbParamCtrls.ResumeLayout(false);
+            this.gbParamCtrls.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHistoryFilePath;
+        private System.Windows.Forms.Label lblHistDataPath;
+        private System.Windows.Forms.Button btnDoCalcs;
+        private System.Windows.Forms.GroupBox gbOutput;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.GroupBox gbParamCtrls;
+        private System.Windows.Forms.TextBox tBoxPrincipalAmt;
+        private System.Windows.Forms.TextBox tBoxSellFee;
+        private System.Windows.Forms.TextBox tBoxPurchaseFee;
+    }
+}
+
---
+++ ForecastGenerator/ForecastGenerator/frmMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace ForecastGenerator
+{
+    public partial class frmMain : Form
+    {
+        private string[] OutColNames = new string[]
+            {
+                "length", "start", "finish", "current", "min", "max", "total change", "avg daily change",
+                "pos days", "neg days"
+            };
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            this.txtHistoryFilePath.Text = appSettings["historyDataPath"];
+            //Set up the cols of the dgv output
+            var dgvOutputControl = (DataGridView)this.gbOutput.Controls["dgvData"];
+            //Length, start, finish, min, max, avg daily change.  Num Increase days, num decrease days.  
+            //Take in current value of btc, total contribution, buy fee, sell fee.
+            SetColumnsForOutput(dgvOutputControl, this.OutColNames);
+            //This is hacky but w/e
+            List<Label> labelsToAdd = new List<Label>();
+            foreach (var ctrl in this.gbParamCtrls.Controls)
+            {
+                TextBox inputCtrl = ctrl as TextBox;
+                Label tempLabel = new Label();
+                labelsToAdd.Add(tempLabel);
+                tempLabel.Name = (ctrl as TextBox).Name.Replace("tBox", "lbl");
+                tempLabel.Location = new Point(inputCtrl.Location.X, inputCtrl.Location.Y - 15);
+                tempLabel.Text = tempLabel.Name.Replace("lbl", "");
+            }
+            this.gbParamCtrls.Controls.AddRange(labelsToAdd.ToArray<Control>());
+        }
+
+        private void SetColumnsForOutput(DataGridView dgvCtrl, string[] colNames)
+        {
+            dgvCtrl.Rows.Clear();
+            //
+            dgvCtrl.Columns.Clear();
+            //
+            //dgvCtrl.Columns.Add("Length", "Length");
+            //dgvCtrl.Columns.Add("Start", "Start");
+            foreach (string colName in colNames)
+            {
+                dgvCtrl.Columns.Add(colName, colName);
+            }
+        }
+
+        
+    }
+}
